55a92fcab43ff2c9c01b2acf30ed31458c986b9b
hopelite <KurdesoWadim@gmail.com>
2022-12-11 21:21:53 +0100

    feat: add hp tracking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D playerRigidBody;
+    float maxSecondsInInvicibility = 0.5f;
+    float currentInvisibilitySeconds;
+    public int currentHp;
+    bool isTakingDamage;
     bool isOnFloor;
 
     public string playerName;
+    public int maxHp = 3;
     public float horizontalSpeed = 50f;
-    public float jumpSpeed = 20f;
-
-
-
-
-    float secondsInInvicibility = 0.5f;
-    float currentInvisibilitySeconds;
-    bool isTakingDamage;
+    public float jumpSpeed = 10f;
+    public List<GameObject> hitPoints;
 
     float minX;
     float maxX;
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
         minY = bottomLeft.y;
         maxY = topRight.y;
 
+        currentHp = maxHp;
         playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -55,22 +56,14 @@ public class PlayerController : MonoBehaviour
         // Move player inside the borders (screen borders), if they leaved them
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY), 0f);
 
-        //if (isTakingDamage)
-        //{
-        //    currentInvisibilitySeconds += Time.deltaTime;
-        //    if (currentInvisibilitySeconds >= secondsInInvicibility)
-        //    {
-        //        currentInvisibilitySeconds = 0.0f;
-        //        isTakingDamage = false;
-        //    }
-        //}
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!isTakingDamage && collision.gameObject.CompareTag("Finger"))
+        if (isTakingDamage)
         {
-            //isTakingDamage = true;
+            currentInvisibilitySeconds += Time.deltaTime;
+            if (currentInvisibilitySeconds >= maxSecondsInInvicibility)
+            {
+                currentInvisibilitySeconds = 0.0f;
+                isTakingDamage = false;
+            }
         }
     }
 
@@ -79,6 +72,30 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Floor"))
         {
             isOnFloor = true;
+        }
+
+        if (!isTakingDamage && collision.gameObject.CompareTag("Finger"))
+        {
+            isTakingDamage = true;
+            currentHp--;
+            if (currentHp >= 0)
+            {
+                if (currentHp != hitPoints.Count - 1)
+                {
+                    Debug.LogError("You forgot to add hit point object to " + playerName + "'s hit points list.");
+                    return;
+                }
+
+                GameObject hitPoint = hitPoints[currentHp];
+                hitPoints.Remove(hitPoint); // Remove life
+                Destroy(hitPoint);
+            }
+
+            if (currentHp <= 0)
+            {
+                // Game over
+                return;
+            }
         }
     }
 
